99e144c768d087175fae367a4713899c379d12b4
ayuktha63 <krishnaprasadsm63@gmail.com>
2025-02-22 03:19:06 +0530

    build

---
+++ build/precache-manifest.a686f56173d9d849bd82d3058155901a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad8a2bcec6a5b1cccf092c21f0770087",
+    "url": "/index.html"
+  },
+  {
+    "revision": "502e837b4f10b2ea8d88",
+    "url": "/static/css/2.83d756e6.chunk.css"
+  },
+  {
+    "revision": "13ab6e00da38fd43b363",
+    "url": "/static/css/main.79ee2bc0.chunk.css"
+  },
+  {
+    "revision": "502e837b4f10b2ea8d88",
+    "url": "/static/js/2.4c414a1c.chunk.js"
+  },
+  {
+    "revision": "d4b4259a290c788aec291821574a2b0b",
+    "url": "/static/js/2.4c414a1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13ab6e00da38fd43b363",
+    "url": "/static/js/main.7ee7e5c0.chunk.js"
+  },
+  {
+    "revision": "67f105f764696c0853b4",
+    "url": "/static/js/runtime-main.cc80a184.js"
+  },
+  {
+    "revision": "b9c33df53620cfe726157cc7b144b6e1",
+    "url": "/static/media/Default.b9c33df5.svg"
+  },
+  {
+    "revision": "f77e5090cfd0fdd77cd703bf5127f323",
+    "url": "/static/media/bulb.f77e5090.png"
+  },
+  {
+    "revision": "6b316a7e4cb4b559de9c81b6209ed0a4",
+    "url": "/static/media/cursor.6b316a7e.cur"
+  },
+  {
+    "revision": "6655aed7ba350b1bcc7da0fa1505267a",
+    "url": "/static/media/events.6655aed7.svg"
+  },
+  {
+    "revision": "a1afe99c59e5309c6c463d741c13e9a2",
+    "url": "/static/media/facebook.a1afe99c.svg"
+  },
+  {
+    "revision": "932cf2c3e36c48632e6f6974e1164630",
+    "url": "/static/media/image1.932cf2c3.png"
+  },
+  {
+    "revision": "41ecb33165697db1bc4360dad815616c",
+    "url": "/static/media/image2.41ecb331.png"
+  },
+  {
+    "revision": "310cc16dc4fdc0cc56b9cdc785b08cea",
+    "url": "/static/media/image3.310cc16d.png"
+  },
+  {
+    "revision": "cc9a69dcc4f3c6fb7eb6793f4e5a8168",
+    "url": "/static/media/instagram.cc9a69dc.svg"
+  },
+  {
+    "revision": "693364a26adbbed001c18970e54eaa52",
+    "url": "/static/media/kiran.693364a2.jpg"
+  },
+  {
+    "revision": "c61396e373999f492b179bb826934267",
+    "url": "/static/media/krishna.c61396e3.jpg"
+  },
+  {
+    "revision": "67cddf2d9f9348df3f70b2bbf19bca38",
+    "url": "/static/media/linkedln.67cddf2d.svg"
+  },
+  {
+    "revision": "bcd8badd648d9be11fc9fc8745704c9f",
+    "url": "/static/media/yash.bcd8badd.png"
+  }
+]);
